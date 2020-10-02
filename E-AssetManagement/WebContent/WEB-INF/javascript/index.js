9405dadb3a58129028dc9623501beae0060b6db1
Varun Gujarathi <v.gujarathi777@gmail.com>
2020-10-02 17:34:28 +0530

    ajax code

--- E-AssetManagement/WebContent/WEB-INF/javascript/index.js
+++ E-AssetManagement/WebContent/WEB-INF/javascript/index.js
@@ -1,29 +1,69 @@
-window.addEventListener('load',function(){
-    let submitBtn = document.getElementById("submit");
-    submitBtn.addEventListener('click', function(){
-        let dataValid = true;
-        let regForm = document.querySelector("#reg_form");
-        let confPasswordAlert = document.getElementById("conf_password_alert");
-        let password = document.getElementById("password");
-        let confPassword = document.getElementById("conf_password");
+/**
+ * @author Varun Gujarathi
+ * @createdOn 02 Oct 2020
+ */
+function submitForm(){
 
-        // PASSWORD VALIDATION
-        // TODO: Check if password is strong
-        // check is password and confirm password fields are matching
-        if(password != confPassword){
-            // TODO: Send data to backend
-            dataValid = false;
-            confPasswordAlert.innerHTML = "PASSWORD DOES NOT MATCH";
-        }
+    let dataValid = true;
 
-        if(dataValid === true){
-            console.log("Sending data to server");
-            sendDataToServer();
-        }
+    // retrieve values of form fields
+    var regForm = document.forms["reg_form"];
+    console.log(regForm);
+    var name = regForm["name"];
+    var telephone =  regForm["telephone"];
+    var email =  regForm["email"];
+    var username =  regForm["username"];
+    var password =  regForm["password"];
+    var confPassword =  regForm["conf_password"];
 
-    })
-});
+    // get p tag elements
+    var nameAlert = document.getElementById("name_alert");
+    var telephoneAlert =  document.getElementById("telephone_alert");
+    var emailAlert =  document.getElementById("email_alert");
+    var usernameAlert =  document.getElementById("username_alert");
+    var passwordAlert =  document.getElementById("password_alert");
+    var confPasswordAlert =  document.getElementById("conf_password_alert");
+
+    var submitBtn =  document.getElementById("submitBtn");
+
+    // PASSWORD VALIDATION
+    // TODO: Check if password is strong
+    // check is password and confirm password fields are matching
+    if(password != confPassword){
+        dataValid = false;
+        confPasswordAlert.innerHTML = "PASSWORD DOES NOT MATCH";
+    }
+
+    if(dataValid === true){
+        console.log("Sending data to server");
+        sendDataToServer();
+    }
+
+}
 
 function sendDataToServer(){
-    
+    var ajax=new XMLHttpRequest();
+    submitBtn.disabled =true;
+    submitBtn.value="Processing...";
+    // TODO: Uncomment the below line and
+    // add the registration form submission endpoint
+
+    //ajax.open("POST","",true);
+    ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    ajax.send("name="+name.value+"&telephone="+telephone.value+"&email="+email.value+"&domain="+domain.value+"&shift="+shift.value+"&timestamp="+currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/"
+                + currentdate.getFullYear() + " @ "
+                + currentdate.getHours() + ":"
+                + currentdate.getMinutes() + ":"
+                + currentdate.getSeconds());
+  ajax.onreadystatechange=function(){
+    if(this.readyState==3){
+      submitButton.value="Submitting...";
+    }
+    else if (this.readyState==4&&this.status==200) {
+      //request ready
+      submitButton.value="Submitted!";
+      document.forms["form"].reset();
+      window.setTimeout(function(){submitButton.disabled =false;submitButton.value="Submit Form";},8000);
+    }
 }
