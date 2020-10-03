4f40473fe9d6f49ca12911111a75a6103623d9ef
Varun Gujarathi <v.gujarathi777@gmail.com>
2020-10-03 11:21:33 +0530

    user and asset classes created

---
+++ E-AssetManagement/src/com/hsbc/easset/models/Asset.java
@@ -0,0 +1,9 @@
+package com.hsbc.easset.models;
+
+public class Asset {
+	private String name;
+	private String assetType;
+	private String description;
+	private LocalDate dateAdded;
+	private boolean isAvailable;
+}
---
+++ E-AssetManagement/src/com/hsbc/easset/models/User.java
@@ -0,0 +1,10 @@
+package com.hsbc.easset.models;
+
+public class User {
+	private String name;
+	private Role role;
+	private long number;
+	private String emailID;
+	private String password;
+	private String username;
+}
