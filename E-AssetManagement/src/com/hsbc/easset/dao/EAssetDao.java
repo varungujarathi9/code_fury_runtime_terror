d3fd79e0d5b638a0966a063fe8b998586cb7bd2c
prachikanakdande <prachikanakdande@gmail.com>
2020-10-04 16:37:01 +0530

    BorrowerShowAvailableAssetController and  show available assets() in dao layer

--- E-AssetManagement/src/com/hsbc/easset/dao/EAssetDao.java
+++ E-AssetManagement/src/com/hsbc/easset/dao/EAssetDao.java
@@ -1,5 +1,8 @@
 package com.hsbc.easset.dao;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import com.hsbc.easset.exceptions.DBConnCreationException;
 import com.hsbc.easset.models.Asset;
 import com.hsbc.easset.models.User;
@@ -9,4 +12,10 @@ public interface EAssetDao {
     boolean validateLogin(User user) throws DBConnCreationException;
     boolean addAsset(Asset asset) throws DBConnCreationException;
 	boolean isAdmin(User user) throws DBConnCreationException; //to redirect to admin or burrower homepage
+	
+	List<Asset> showAvailableAssets() throws SQLException;
+	//List<Asset> searchAvailableAssets(Asset asset);
+	//boolean issueAvailableAssets(List<Asset> assetList);
+	//boolean returnAssets(List<Asset> assetList);
+	
 }
--- E-AssetManagement/src/com/hsbc/easset/models/Asset.java
+++ E-AssetManagement/src/com/hsbc/easset/models/Asset.java
@@ -3,6 +3,7 @@ package com.hsbc.easset.models;
 import java.time.LocalDate;
 
 public class Asset {
+	
 	private String name;
 	private String assetType;
 	private String description;
